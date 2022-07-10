dd35b1d3196f0803502a2d1f65e81861afc2c90c
Payam Firouzfar <78821356+payamfirouzfar@users.noreply.github.com>
2022-07-10 10:19:53 +0430

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+--select * from EmployeeInfo
+--inner join Qualification on EmployeeInfo.QualificationID=Qualification.QualificationID
+
+
+--select * from EmployeeInfo
+--left outer join Qualification on EmployeeInfo.QualificationID=Qualification.QualificationID
+
+
+--Select * from EmployeeInfo
+--right outer join Qualification on EmployeeInfo.QualificationID=Qualification.QualificationID
+
+
+Select * from EmployeeInfo
+full outer join Qualification on EmployeeInfo.QualificationID=Qualification.QualificationID
